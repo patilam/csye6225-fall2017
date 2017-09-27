01c86ea5ff23a4d577f1914d5cdfb75e4e08eb84
ALI ASGAR <amar.a@husky.neu.edu>
2017-09-27 15:58:04 -0400

    Application deleted

--- src/main/java/com/csye6225/demo/controllers/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.csye6225.demo.controllers;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<user, Long> {
-
-    }
-
